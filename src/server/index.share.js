67f736fcdf5c107e1d77864b085fbaaabda41d2f
ale-cristofori <compatt84@gmail.com>
2021-01-03 01:58:49 +0000

    corrected static folder path

--- src/server/index.share.js
+++ src/server/index.share.js
@@ -1,43 +1,43 @@
-// Import required AWS SDK clients and commands for Node.js
+//HTTP handling
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require("path");
 const cors = require('cors');
-const http = require('http');
+//Amazon AWS SDK
 const AWS = require('aws-sdk');
+//MapBox Vector Tiles handling
 const geojsonvt = require('geojson-vt');
 const vtpbf = require('vt-pbf');
 
-  // Set the AWS region
+// set the AWS region
 const REGION = "<you AWS region here>"; // e.g., "us-east-1"
-
-// Set the bucket parameters
+// set the bucket parameters
 const bucketName = "<your bucket name here>";
-
-//Create S3 initial config for connection
+// create S3 initial config for connection
 const config = {
     apiVersion: '<preferred API version>',
     accessKeyId: '<IAM AWS account key ID>', 
     secretAccessKey: '<IAM AWS secret key>', 
     region: REGION
 };
-
 const s3 = new AWS.S3(config);
-
 const getParams = {
     Bucket: bucketName, 
-    Key: "telephone_districts.json"
+    Key: "provinces_4326.geojson"
 };
 
-// Create a new express app instance
+// create a new express app instance
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.static("build"));
-
+// we will server the built bundles from the "build" folder
+app.use(express.static(path.join(__dirname, '../../build')));
+/* we use the body parser middleware in case we 
+want to handle response in JSON formats later on */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 let tileIndex;
-
+// route for the Vector Tiles request
 app.get('/tileserver', cors() , (req, res) => {
     const z = parseInt(req.query.z);
     const x = parseInt(req.query.x);
@@ -51,11 +51,14 @@ app.get('/tileserver', cors() , (req, res) => {
     res.send(buffer);
 });
 
+/*open the connection to S3 and get 
+the data from the bucket*/
 s3.getObject(getParams, (error, data) => {
     if (error) console.log(error, error.stack);
     else {
         let jsonData = data.Body.toString('utf-8');
-        jsonData = JSON.parse(jsonData)
+        jsonData = JSON.parse(jsonData);
+        // we tile the full geojson file
         tileIndex = geojsonvt(jsonData);
         app.listen(PORT, () => {
             console.log('App is listening on port 5000!');
