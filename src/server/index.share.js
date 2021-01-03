d4e0a1e27d28a624fd8b94eee4fc2b164aaaaa7f
ale-cristofori <compatt84@gmail.com>
2021-01-03 20:48:42 +0000

    commit for heroku

--- src/server/index.share.js
+++ src/server/index.share.js
@@ -1,43 +1,43 @@
-//HTTP handling
+// Import required AWS SDK clients and commands for Node.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require("path");
 const cors = require('cors');
-//Amazon AWS SDK
+const path = require("path");
 const AWS = require('aws-sdk');
-//MapBox Vector Tiles handling
 const geojsonvt = require('geojson-vt');
 const vtpbf = require('vt-pbf');
 
-// set the AWS region
-const REGION = "<you AWS region here>"; // e.g., "us-east-1"
-// set the bucket parameters
-const bucketName = "<your bucket name here>";
-// create S3 initial config for connection
+  // Set the AWS region
+const REGION = process.env.AWS_REGION; // e.g., "us-east-1"
+
+// Set the bucket parameters
+const bucketName = process.env.BUCKET_NAME;
+
+//Create S3 initial config for connection
 const config = {
-    apiVersion: '<preferred API version>',
-    accessKeyId: '<IAM AWS account key ID>', 
-    secretAccessKey: '<IAM AWS secret key>', 
+    apiVersion: 'latest',
+    accessKeyId: PROCESS.env.S3_ACCESS_KEY_ID, 
+    secretAccessKey: process.env.S3_SECRET_KEY, 
     region: REGION
 };
+
 const s3 = new AWS.S3(config);
+
 const getParams = {
     Bucket: bucketName, 
-    Key: "provinces_4326.geojson"
+    Key: process.env.S3_RESOURCE_NAME
 };
 
-// create a new express app instance
+// Create a new express app instance
 const app = express();
 const PORT = process.env.PORT || 5000;
-// we will server the built bundles from the "build" folder
 app.use(express.static(path.join(__dirname, '../../build')));
-/* we use the body parser middleware in case we 
-want to handle response in JSON formats later on */
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 let tileIndex;
-// route for the Vector Tiles request
+
 app.get('/tileserver', cors() , (req, res) => {
     const z = parseInt(req.query.z);
     const x = parseInt(req.query.x);
@@ -51,14 +51,11 @@ app.get('/tileserver', cors() , (req, res) => {
     res.send(buffer);
 });
 
-/*open the connection to S3 and get 
-the data from the bucket*/
 s3.getObject(getParams, (error, data) => {
     if (error) console.log(error, error.stack);
     else {
         let jsonData = data.Body.toString('utf-8');
         jsonData = JSON.parse(jsonData);
-        // we tile the full geojson file
         tileIndex = geojsonvt(jsonData);
         app.listen(PORT, () => {
             console.log('App is listening on port 5000!');
