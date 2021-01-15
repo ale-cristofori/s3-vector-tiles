8832de881d2101b13676d259fa77097762cae9e5
ale-cristofori <compatt84@gmail.com>
2021-01-15 00:35:55 +0000

    implemented get feature info

--- src/client/components/MapComponent.jsx
+++ src/client/components/MapComponent.jsx
@@ -8,13 +8,16 @@ import MVT from 'ol/format/MVT'
 import OSM from 'ol/source/OSM';
 import VectorTileSource from 'ol/source/VectorTile'
 import {Fill, Stroke, Circle, Style} from 'ol/style';
+import Select from 'ol/interaction/Select';
+import Overlay from 'ol/Overlay';
 import * as olProj from 'ol/proj';
 
 const MapComponent = (props) => {
 
   // set intial state - used to track references to OpenLayers 
   //  objects for use in hooks, event handlers, etc.
-  const [ map, setMap ] = useState()
+  const [ map, setMap ] = useState(null)
+  const [vectorLayer, setVectorLayer] = useState(null)
 
   // get ref to div element - OpenLayers will render into this div
   const mapRef = useRef()
@@ -33,10 +36,15 @@ const MapComponent = (props) => {
             stroke: new Stroke({
               color: 'rgba(30, 30, 30, 0.5)',
               width: 2
-            })
+            }),
+            fill: new Fill({
+              color: 'rgba(15, 85, 1, 0.4)',
+            }),
           })
         }
       })
+
+    setVectorLayer(vectorLayer);
 
     // create map
     const initialMap = new Map({
@@ -48,8 +56,8 @@ const MapComponent = (props) => {
         vectorLayer
       ],
       view: new View({
-        center: olProj.fromLonLat([13, 45]),
-        zoom: 4
+        center: olProj.fromLonLat([13, 42]),
+        zoom: 6
       }),
       controls: []
     });
@@ -61,6 +69,21 @@ const MapComponent = (props) => {
     //it means the useEffect code will only run once when
     //the component will mount 
   },[])
+
+  useEffect(() => {
+    const singleSelectClick = new Select()
+    if(map) {
+      map.on('click', (e) => {
+        const features = map.getFeaturesAtPixel(e.pixel);
+        if(features.length > 0) {
+          const properties = features[0].getProperties();
+          alert(
+            `distreto: ${properties['distretto']} \ncompartimento: ${properties['compartimento']} \nprefisso: ${properties['prefisso']}`
+          )
+        }
+      })
+    }
+  } ,[map])
 
   // render component
   return (      
